031bca4fe3e6c6049c3283bc47357db049a8e318
N. Zonca <noamforspam@gmail.com>
2018-02-20 19:32:34 +0100

    test
    test
    
    ...adding more BS

--- mainRecog.c
+++ mainRecog.c
@@ -1,4 +1,4 @@
-#include <stdlib.h> 
+#include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
 #include <math.h>
@@ -12,20 +12,20 @@ int main(int argc, char *argv[]) {
 
 
 
-/* 
+/*
  * CHANGES:
- * 
+ *
  * 1. recognizeEquations:
- * 		in main change recognizeExpression for new recognizeEquation. The latter will compare whether if the input is <expression> '=' <expression> 
+ * 		in main change recognizeExpression for new recognizeEquation. The latter will compare whether if the input is <expression> '=' <expression>
  * 		- Create new recognizeEquation.c file and its header. (don't just define it here.)
- * 		- 
- * 
+ * 		-
+ *
  * 2. in recognizeExpression:
  * 		- have to add the recognition of ^number ...
- * 		- change print statements to return 1 or 0. 
+ * 		- change print statements to return 1 or 0.
  * 		- *readInput:
- * 			needs, to scan not for whole line, but until equal sign. 
- * 		-
- * 
- * 
+ * 			needs, to scan not for whole line, but until equal sign.
+ * 		- balbhablakb
+ *
+ *  more BS
  * */
